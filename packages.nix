ca46f448f76df9f9ce79b886b49507b2fbd690d7
mintylotl <mintyaxolotl@proton.me>
2024-11-11 01:44:43 +0200

    2024-11-11T01:44:43

--- packages.nix
+++ packages.nix
@@ -16,7 +16,6 @@ in {
         enableCuda = true;
       };
       blender = pkgs-lsg.blender;
-      wineWowPackages = inputs.Hyprland.inputs.nixpkgs.legacyPackages.${pkgs.system}.wineWowPackages;
     })
   ];
 
