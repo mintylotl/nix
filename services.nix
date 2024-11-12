2ef8b9d9e840952a2bd589744f1bab683592c023
mintylotl <mintyaxolotl@proton.me>
2024-11-12 22:42:38 +0200

    2024-11-12T22:42:38

---
+++ services.nix
@@ -0,0 +1,17 @@
+{ config, ... }:
+{
+  systemd = {
+    user.services.hyprauthagent = {
+      description = "Hyprland Authentication Agent";
+      wantedBy = [ "hyprland-session.target" ];
+      wants = [ "hyprland-session.target" ];
+      after = [ "hyprland-session.target" ];
+
+      serviceConfig = {
+        Type = "simple";
+	ExecStart = "${pkgs.hyprpolkitagent}/libexec/hyprpolkitagent";
+	Restart = "on-failure";
+      };
+    };
+  };
+}
