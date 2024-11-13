d4b199968b8822aa926ad1530af37600b246a9c0
mintylotl <mintyaxolotl@proton.me>
2024-11-13 20:42:06 +0200

    2024-11-13T20:42:06

--- dots/scripts/programs/musicbee/musicbee.sh
+++ dots/scripts/programs/musicbee/musicbee.sh
@@ -2,4 +2,4 @@
 #WINEPREFIX="/home/jwm/.musicbee" wine "/home/jwm/.musicbee/drive_c/programs/MusicBee/MusicBee.exe"&
 cd $HOME/.scripts/programs/musicbee
 WINEPREFIX="$HOME"/.bottles/musicB WINEFSYNC=1 PIPEWIRE_LATENCY=512/48000 wine "./.MusicBee/MusicBee.exe"
-sudo ./prio.sh
+sudo -E ./prio.sh
