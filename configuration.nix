a77bdfdc0dbacf2ce1233615da288fc859534d3d
mintylotl <mintyaxolotl@proton.me>
2024-11-13 05:21:42 +0200

    2024-11-13T05:21:42

--- configuration.nix
+++ configuration.nix
@@ -159,7 +159,7 @@ in
   
   services.udev.extraRules = ''
     SUBSYSTEMS=="pci", KERNELS=="0000:2b:00.1", ATTRS{device}=="0x0fb8", ATTRS{enable}="0"
-    SUBSYSTEMS=="pci", KERNELS=="0000:2b:00.4", ATTRS{device}=="0x1487", ATTRS{index}="0"
+    SUBSYSTEMS=="pci", KERNELS=="0000:2b:00.4", ATTRS{device}=="0x1487", DRIVERS="snd_hda_intel_0"
 
 
   '';
