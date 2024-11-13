122db3f500e61878ba264a5a8ecdeab1a82283a7
mintylotl <mintyaxolotl@proton.me>
2024-11-13 07:40:15 +0200

    2024-11-13T07:40:15

--- dots/scripts/programs/musicbee/launcher.sh
+++ dots/scripts/programs/musicbee/launcher.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Check if program is running, if yes, kill tray
 cd /home/jwm/.scripts/programs/musicbee/
-sudo -E ./musicbee.sh&
+./musicbee.sh&
 while [ "$(pgrep -f MusicBee.exe)" == "" ]
 do
 sleep 1s
