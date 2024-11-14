b6db6a6667c48d71bf354a99c855f139a91f75b7
mintylotl <mintyaxolotl@proton.me>
2024-11-14 20:13:28 +0200

    2024-11-14T20:13:28

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
     };
 
     umuProton = {
-      url = "git+https://github.com/Open-Wine-Components/umu-launcher/?dir=packaging\\/nix&submodules=1";
+      url = "git+https://github.com/Open-Wine-Components/umu-launcher/?dir=packaging\/nix&submodules=1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
