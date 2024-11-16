05184cb2427d0f8a5b54b1a653c5b32eff3d87a5
mintylotl <mintyaxolotl@proton.me>
2024-11-16 23:35:05 +0200

    2024-11-16T23:35:05

--- dots/scripts/programs/musicbee/musicbee.sh
+++ dots/scripts/programs/musicbee/musicbee.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd $HOME/.scripts/programs/musicbee
-WINEPREFIX="$HOME"/.bottles/musicB PULSE_LATENCY_MSEC=200 wine "./.MusicBee/MusicBee.exe"
+WINEPREFIX="$HOME"/.bottles/musicB PULSE_LATENCY_MSEC=50 wine "./.MusicBee/MusicBee.exe"
 sudo -E ./prio.sh
