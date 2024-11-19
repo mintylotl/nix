5eaf24288a930f92061d6c5552df6f9a7550dbed
mintylotl <mintyaxolotl@proton.me>
2024-11-19 18:06:25 +0200

    2024-11-19T18:06:25

--- dots/scripts/ffmpeg/transcode/AVC.sh
+++ dots/scripts/ffmpeg/transcode/AVC.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-for x in ./input/*
-do
-	ffmpeg -i "$x" -c:v libx264 -preset slow -pix_fmt yuv420p -crf 22 -c:a aac -b:a 256k -filter_complex "scale=720:-1:flags=lanczos[v]" -map "[v]" -map 0:a ./output/"$(basename "$x")".mp4
+for x in ./input/*; do
+    ffmpeg -i "$x" -c:v libx264 -preset slow -pix_fmt yuv420p -crf 22 -c:a aac -b:a 256k -filter_complex "scale=1280:-1:flags=lanczos[v]" -map "[v]" -map 0:a ./output/"$(basename "$x")".mp4
 done
