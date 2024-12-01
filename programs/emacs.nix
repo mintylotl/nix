1bd4e30923ee36a9411d3b70f8539867890909d3
mintylotl <mintyaxolotl@proton.me>
2024-12-01 04:32:34 +0200

    2024-12-01T04:32:34

---
+++ programs/emacs.nix
@@ -0,0 +1,42 @@
+{ config, lib, pkgs, ... }:
+
+{
+  programs.emacs = {
+    enable = true;
+    package = pkgs.emacs29-pgtk;
+    extraPackages = epkgs:
+      with epkgs; [
+        vterm
+        fd
+        ripgrep
+        cmake
+        clang
+        jsbeautifier
+        nixfmt-classic
+        ispell
+        gnuplot
+        shfmt
+        gnumake
+        pipenv
+        cmigemo
+        stylelint
+        shellcheck
+        zig
+        graphviz
+        pandoc
+        libtool
+        rustup
+        rust-analyzer
+        pnpm
+        html-tidy
+        sbcl
+      ];
+  };
+  services.emacs = {
+    enable = true;
+    client.enable = true;
+    defaultEditor = true;
+
+    startWithUserSession = false;
+  };
+}
