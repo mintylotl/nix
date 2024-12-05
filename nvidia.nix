f309c38b91c9de6ac2ee685ba35c1e0b58164552
mintylotl <mintyaxolotl@proton.me>
2024-12-05 19:14:57 +0200

    2024-12-05T19:14:57

--- nvidia.nix
+++ nvidia.nix
@@ -2,19 +2,11 @@
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
-    extraPackages = with pkgs; [
-      libva-vdpau-driver
-      nvidia-vaapi-driver
-      libva-utils
-    ];
   };
 
   services.xserver = {
-    videoDrivers = [ "nvidia" "fbdev" ];
     enable = true;
   };
-
-  services.desktopManager.plasma6.enable = true;
 
   hardware.nvidia = {
     modesetting.enable = true;
