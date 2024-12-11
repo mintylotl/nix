acc2d2691a7d18b0c327e1ee1869d7420179fca1
mintylotl <mintyaxolotl@proton.me>
2024-12-11 21:46:02 +0200

    gameboi

---
+++ home_gb.nix
@@ -0,0 +1,75 @@
+{ config, pkgs, lib, ... }:
+let HOME = "/home/jwm";
+in {
+  imports = [ ./programs/zsh.nix ];
+
+  home.username = "gameboy";
+  home.homeDirectory = "/home/gameboy";
+  home.preferXdgDirectories = true;
+  programs.home-manager.enable = true;
+
+  home.sessionVariables = { NIXOS_OZONE_WL = "1"; };
+
+  home.file = {
+    ".config/pipewire" = {
+      source = ./dots/config/pipewire;
+      recursive = true;
+    };
+    ".asoundrc" = { source = ./dots/config/asoundrc; };
+  };
+
+  # Home Stuff
+  xdg = {
+    enable = true;
+    mime.enable = true;
+    mimeApps.enable = false;
+
+    portal = {
+      enable = true;
+      xdgOpenUsePortal = true;
+      extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+      config.common.default = "gtk";
+    };
+
+    configHome = "${HOME}/.config";
+    cacheHome = "${HOME}/.cache";
+    dataHome = "${HOME}/.local/share";
+    stateHome = "${HOME}/.local/state";
+
+    userDirs.createDirectories = true;
+  };
+
+  # Programs
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+
+    enableBashIntegration = true;
+  };
+
+  programs.bash = {
+    enable = true;
+    initExtra = ''
+      zsh
+    '';
+  };
+
+  programs.neovim = { enable = true; };
+
+  programs.kitty.enable = true;
+  programs.alacritty.enable = true;
+  programs.rofi = {
+    enable = true;
+    theme = ./config/dracula.rasi;
+    terminal = "${pkgs.alacritty}/bin/alacritty";
+  };
+
+  qt = {
+    enable = true;
+    platformTheme.name = "qtct";
+    style.name = "kvantum";
+    style.package = pkgs.qt6Packages.qtstyleplugin-kvantum;
+  };
+
+  home.stateVersion = "24.05";
+}
