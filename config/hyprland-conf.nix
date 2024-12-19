97a4cf8b395d366169b9859fbd1b89d954a93cfb
mintylotl <mintyaxolotl@proton.me>
2024-12-19 17:08:00 +0200

    Stable

--- config/hyprland-conf.nix
+++ config/hyprland-conf.nix
@@ -44,11 +44,10 @@
       # --Scripts
       exec-once = "$HOME"/.config/hypr/sink.sh
       exec-once = sudo -E "$scripts"/system/leds.sh&
-      exec-once = sudo -E "$scripts"/system/mounts.sh
+      exec-once = sudo -E "$scripts"/system/mounts.sh && emacs --daemon
 
       # --Programs
       exec-once = hyprpaper
-      exec-once = emacs --daemon
 
       # --Lutris
       env = LUTRIS_SKIP_INIT, 1
