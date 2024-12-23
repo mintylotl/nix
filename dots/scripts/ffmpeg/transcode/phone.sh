ac6382e2cdd048beec9bef22eeaad298ad5d221d
mintylotl <mintyaxolotl@proton.me>
2024-12-23 20:36:12 +0200

    Change ffmpeg scripts

--- dots/scripts/ffmpeg/transcode/phone.sh
+++ dots/scripts/ffmpeg/transcode/phone.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-for x in ./input/*
-do
-	ffmpeg -i "$x" -c:v libx265 -preset fast -profile:v main10 -pix_fmt yuv420p10le -crf 26 -c:a libopus -b:a 88k -vbr on -compression_level 10 ./output/"$(basename "$x")".mkv
+for x in ./input/*; do
+    ffmpeg -i "$x" -c:v libx264 -preset fast -profile:v main -pix_fmt yuv420p -crf 26 -c:a libopus -b:a 88k -vbr on -compression_level 10 ./output/"$(basename "$x")".mp4
 done
