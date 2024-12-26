98860ffa181070fcb20a2eea1b65099231c9e4d3
mintylotl <mintyaxolotl@proton.me>
2024-12-27 00:18:20 +0200

    Emacs Service

--- services.nix
+++ services.nix
@@ -1,4 +1,15 @@
 { config, pkgs, ... }: {
-  systemd = { services = { }; };
+  systemd = {
+    services = {
+      emacs = {
+        wantedBy = [ "multi-user.target" ];
+        Description = "Emacs Daemon Service";
+
+        serviceConfig = {
+          ExecStart = ''echo "${pkgs.emacs-pgtk-with-packages}"'';
+        };
+      };
+    };
+  };
   services.udisks2.enable = true;
 }
