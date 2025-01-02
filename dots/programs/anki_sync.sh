5186d8859981f907ec01f7d21c5f88f383247189
mintylotl <mintyaxolotl@proton.me>
2025-01-02 06:31:27 +0200

    Anki

--- dots/programs/anki_sync.sh
+++ dots/programs/anki_sync.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-export SYNC_PORT="37322"
-export SYNC_USER1="Anki:85878"
+export SYNC_PORT="37355"
+export SYNC_USER1="Anki:Anki240499"
 export SYNC_BASE="/home/jwm/.AnkiSync"
 #export SYNC_ENDPOINT_MEDIA="http://127.0.0.1:27701/msync/"
 
-anki --syncserver
-
+anki-sync-server
