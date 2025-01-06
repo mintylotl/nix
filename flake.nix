810bb51ee86fc6497ea2de65ac65f8266c2cab09
mintylotl <mintyaxolotl@proton.me>
2025-01-06 02:26:34 +0200

    ffmpeg scripts

--- flake.nix
+++ flake.nix
@@ -12,10 +12,10 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs_unstable = { url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
-    nixpkgs = { url = "github:nixos/nixpkgs?ref=nixos-24.11"; };
+    nixpkgs = { url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
 
     home-manager = {
-      url = "github:nix-community/home-manager?ref=release-24.11";
+      url = "github:nix-community/home-manager?ref=master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
