907fc856a7ac83a6a1709bce7ede53c842c700a6
mintylotl <mintyaxolotl@proton.me>
2025-01-21 23:22:23 +0200

    flake:umu

--- flake.nix
+++ flake.nix
@@ -25,8 +25,8 @@
     };
 
     umuProton = {
-      url =
-        "github:Open-Wine-Components/umu-launcher/59a82ea8cd284c7535bc06b8f6156abb7da96f6a?dir=packaging/nix";
+      url = "github:Open-Wine-Components/umu-launcher?dir=packaging/nix";
+      #"github:Open-Wine-Components/umu-launcher/59a82ea8cd284c7535bc06b8f6156abb7da96f6a?dir=packaging/nix";
     };
 
     Hyprland = {
