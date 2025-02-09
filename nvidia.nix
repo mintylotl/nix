5299b12abeed903262a4fba274721e9dabe988df
mintylotl <mintyaxolotl@proton.me>
2025-02-09 17:49:34 +0200

    nvidia:buffer pool

--- nvidia.nix
+++ nvidia.nix
@@ -29,4 +29,7 @@
     powerManagement.finegrained = false;
     powerManagement.enable = true;
   };
+
+  environment.etc."nvidia/nvidia-application-profiles-rc.d/50-limit-free-buffer-pool-in-wayland-compositors.json".source =
+    ./resources/50-wayland-buffer-pool.json;
 }
