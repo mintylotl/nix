c2eaf5965c275e7c0a92b624bf274a07f6c846cb
mintylotl <mintyaxolotl@proton.me>
2025-02-10 20:01:27 +0200

    config:hyprland:explicit_sync: 1 -> 0

--- nvidia.nix
+++ nvidia.nix
@@ -3,8 +3,8 @@
   hardware.graphics = {
     enable = lib.mkDefault true;
     enable32Bit = lib.mkDefault true;
-    extraPackages = with pkgs; [ libva-vdpau-driver egl-wayland ];
-    extraPackages32 = with pkgs.driversi686Linux; [ libva-vdpau-driver ];
+    extraPackages = with pkgs; [ ];
+    extraPackages32 = with pkgs.driversi686Linux; [ ];
   };
 
   services.xserver = {
