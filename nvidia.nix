837ad1a2a29a63668074ac0b118890a728989e9a
mintylotl <mintyaxolotl@proton.me>
2025-02-19 01:20:52 +0200

    nvidia

--- nvidia.nix
+++ nvidia.nix
@@ -4,6 +4,7 @@
     enable = lib.mkDefault true;
     enable32Bit = lib.mkDefault true;
     extraPackages = [ pkgs.libva-vdpau-driver ];
+    extraPackages32 = [ pkgs.i686-linux.libva-vdpau-driver ];
   };
 
   services.xserver = {
