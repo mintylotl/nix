b5b4cc288841d7d6922228667b8fbf6a312c40f1
mintylotl <mintyaxolotl@proton.me>
2025-02-19 02:59:07 +0200

    flake:update

--- flake.nix
+++ flake.nix
@@ -7,6 +7,13 @@
     max-jobs = 1;
     max-substitution-jobs = 1;
     cores = 5;
+
+    extra-substituters =
+      [ "https://nix-community.cachix.org" "https://cache.nixos.org/" ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "prismlauncher.cachix.org-1:9/n/FGyABA2jLUVfY+DEp4hKds/rwO+SCOtbOkDzd+c="
+    ];
   };
 
   inputs = {
