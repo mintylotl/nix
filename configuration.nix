2517c022b3fd28a6ea40f9de0e22f889a6905ee8
mintylotl <mintyaxolotl@proton.me>
2025-02-19 03:11:55 +0200

    flake:update

--- configuration.nix
+++ configuration.nix
@@ -39,17 +39,7 @@ in {
       automatic = false;
       dates = [ "06:00" ];
     };
-    settings = {
-      trusted-users = [ "jwm" ];
-      trusted-substituters = [ "https://prismlauncher.cachix.org" ];
-
-      substituters =
-        [ "https://nix-community.cachix.org" "https://cache.nixos.org/" ];
-      trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "prismlauncher.cachix.org-1:9/n/FGyABA2jLUVfY+DEp4hKds/rwO+SCOtbOkDzd+c="
-      ];
-    };
+    settings = { trusted-users = [ "jwm" ]; };
   };
 
   documentation = {
