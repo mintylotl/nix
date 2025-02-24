3d91fe1f5f54b9ab15270308fd0268df54cc3dc3
mintylotl <mintyaxolotl@proton.me>
2025-02-24 15:43:27 +0200

    dots:ffmpeg:AVC

--- dots/scripts/ffmpeg/transcode/AVC.sh
+++ dots/scripts/ffmpeg/transcode/AVC.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 for x in ./input/*; do
-    ffmpeg -i "$x" -c:v libx264 -preset superfast -pix_fmt yuv420p -crf 24 -c:a libopus -b:a 60k -filter_complex "scale=1280:-1:flags=lanczos[v]" -map "[v]" -map 0:a ./output/"$(basename "$x")".mkv
+    ffmpeg -i "$x" -c:v libx264 -preset faster -pix_fmt yuv420p -crf 19 -c:a libopus -b:a 256k -filter_complex "scale=1280:-1:flags=lanczos[v]" -map "[v]" -map 0:a:1 -map 0:s ./output/"$(basename "$x")".mkv
 done
