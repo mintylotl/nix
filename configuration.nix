b747ffa40f1cad9039b5cc2a3c2c70a7d2df8070
mintylotl <mintyaxolotl@proton.me>
2025-03-01 22:17:04 +0200

    yt_scripts;locales

--- configuration.nix
+++ configuration.nix
@@ -216,7 +216,8 @@ in {
   # IME
   i18n = {
     defaultLocale = "en_US.UTF-8";
-    supportedLocales = [ "en_US.UTF-8" "ja_JP.UTF-8" "ko_KR.UTF-8" ];
+    supportedLocales =
+      [ "en_US.UTF-8/UTF-8" "ja_JP.UTF-8/UTF-8" "ko_KR.UTF-8/UTF-8" ];
 
     inputMethod = {
       enable = true;
