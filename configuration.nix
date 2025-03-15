bfbd1706d1ef2c58e4d69798a41080e980d491c0
mintylotl <mintyaxolotl@proton.me>
2025-03-16 01:07:25 +0200

    nvidia:stable->beta

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,7 @@
 { nixpkgs, config, lib, pkgs, inputs, ... }:
 let
   HOME = "/home/jwm";
-  nvidia = config.boot.kernelPackages.nvidiaPackages.stable;
+  nvidia = config.boot.kernelPackages.nvidiaPackages.beta;
   cuda = pkgs.cudaPackages.cudatoolkit;
 in {
   # nixOS
