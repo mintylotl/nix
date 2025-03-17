9e6b6cc139022d4fd19594a54e26a9d3bb0ffbe0
mintylotl <mintyaxolotl@proton.me>
2025-03-18 01:10:26 +0200

    xdg:emacsclient

--- home/xdg.nix
+++ home/xdg.nix
@@ -72,8 +72,9 @@ in {
 
         icon = "emacs";
         type = "Application";
-        exec = ''
-          emacsclient --alternate-editor= -s "${HOME}/.emacs.d/emacflurry/mcflurry" -r %F'';
+        #exec = ''
+        #  WAYLAND_DISPLAY=$WAYLAND_DISPLAY emacsclient --alternate-editor= -s "${HOME}/.emacs.d/emacflurry/mcflurry" -c %F
+        #'';
 
         terminal = false;
         categories = [ "Development" "TextEditor" ];
@@ -81,13 +82,14 @@ in {
 
         settings = {
           Keywords = "emacsclient";
-          StartupWMClass = "Emacs";
+          StartupWMClass = "Emacsd";
         };
 
         actions."new-window" = {
           name = "New Frame";
           exec = ''
-            emacsclient --alternate-editor= -s "${HOME}/.emacs.d/emacflurry/mcflurry" -c %F'';
+            emacsclient --alternate-editor= -s "${HOME}/.emacs.d/emacflurry/mcflurry" -c %F
+          '';
         };
       };
     };
