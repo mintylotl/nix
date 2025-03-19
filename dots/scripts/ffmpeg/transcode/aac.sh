27d6199cbc29bb5362795296fdbc42b474ca7183
mintylotl <mintyaxolotl@proton.me>
2025-03-20 01:02:54 +0200

    scripts:ffmpeg:aac

--- dots/scripts/ffmpeg/transcode/aac.sh
+++ dots/scripts/ffmpeg/transcode/aac.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 for x in ./input/*; do
-    ffmpeg -n -i "$x" -c:a aac -b:a 192k -map 0:a:0 ./output/"$(basename "$x")".m4s
+    ffmpeg -n -i "$x" -c:a aac -b:a 192k -map 0:a:0 ./output/"$(basename "$x")".m4a
 done
