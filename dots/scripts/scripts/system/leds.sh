9bc6d45184b343946090a9357a9a95e4ad1d8e49
mintylotl <mintyaxolotl@proton.me>
2025-03-21 11:54:13 +0200

    scripts:leds

--- dots/scripts/scripts/system/leds.sh
+++ dots/scripts/scripts/system/leds.sh
@@ -3,14 +3,14 @@
 DEV=""
 for x in /sys/class/leds/*;
 do
-	if printf "$x" | grep -q "scrolllock"
-	then
-		DEV=$(basename "$x")
-	fi
+    if printf "$x" | grep -q "scrolllock"
+    then
+        DEV=$(basename "$x")
+    fi
 done
 
 while :
 do
     brightnessctl -q --device="$DEV" set 1
-    sleep 0.01s
+    sleep 0.1s
 done
