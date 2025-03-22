c593a492745bafa4bd4a53d151505d17c287ecae
mintylotl <mintyaxolotl@proton.me>
2025-03-22 03:30:05 +0200

    musicbee:shortcuts

--- dots/programs/musicbee/shortcuts/close_kill.sh
+++ dots/programs/musicbee/shortcuts/close_kill.sh
@@ -3,12 +3,11 @@
 cd "$HOME/.scripts/programs/musicbee/shortcuts" || exit
 WINDOW="$(./getWdName.sh)"
 
-xdotool key --window "$WINDOW" "SUPER+Delete"
+xdotool key --window "$WINDOW" "Delete"
 sleep 1s
 
 VALUE="$(ps aux | grep "explorer" | grep -v "grep" | awk '{print $2}')"
 if [[ "$VALUE" != "" && -e ./.startup ]]; then
-    killall explorer.exe
-    #kill -9 $VALUE
+    kill -9 $VALUE
     rm ./.startup
 fi
