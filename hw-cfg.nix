dd2b3a2e1c5c4c9260701e7bc527d009aee23cac
mintylotl <mintyaxolotl@proton.me>
2025-03-25 07:17:43 +0200

    swap

--- hw-cfg.nix
+++ hw-cfg.nix
@@ -34,8 +34,6 @@
     options = [ "fmask=0022" "dmask=0022" ];
   };
 
-  swapDevices = [ ];
-
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode =
     lib.mkDefault config.hardware.enableRedistributableFirmware;
