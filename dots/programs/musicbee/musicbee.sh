2cff3f73c05b67f05fed41c49eb4aed258727074
mintylotl <mintyaxolotl@proton.me>
2025-04-28 06:27:44 +0200

    pipewire

--- dots/programs/musicbee/musicbee.sh
+++ dots/programs/musicbee/musicbee.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd $HOME/.scripts/programs/musicbee
 
-WINEPREFIX="$HOME"/.bottles/musicB PULSE_LATENCY_MSEC=1024 $mbWINE "./.MusicBee/MusicBee.exe"
+WINEPREFIX="$HOME"/.bottles/musicB PULSE_SINK=alsa-headphones $mbWINE "./.MusicBee/MusicBee.exe"
 sudo -E ./prio.sh
