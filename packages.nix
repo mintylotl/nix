d6340ffc31fc6886d8229ac16fdbbee65c50ac96
mintylotl <mintyaxolotl@proton.me>
2025-08-13 01:27:45 +0200

    scripts:garb

--- packages.nix
+++ packages.nix
@@ -228,6 +228,15 @@ in
         gamescope
         gamemode
         mangohud
+        vulkan-tools
+        vulkan-loader
+        gst_all_1.gstreamer
+        gst_all_1.gst-vaapi
+        gst_all_1.gst-plugins-base
+        gst_all_1.gst-plugins-good
+        gst_all_1.gst-plugins-ugly
+        gst_all_1.gst-plugins-bad
+        gst_all_1.gst-libav
       ];
     }))
     protontricks
