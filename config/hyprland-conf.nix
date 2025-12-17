562e3a96b42985806d6ac42769ddb320bea4689a
mintylotl <mintyaxolotl@proton.me>
2025-12-17 20:25:55 +0200

    hypr: session terminate

--- config/hyprland-conf.nix
+++ config/hyprland-conf.nix
@@ -238,7 +238,7 @@
         #bind = $mainMod, R, workspaceopt, allfloat
 
         # Misc
-        "$mainMod, M, exec, loginctl terminate-session \"\""
+        "$mainMod, M, exec, bash -c 'umount ~/.orgnotes; loginctl terminate-session \"\"'"
         "$mainMod CTRL, down, exec, bash -c 'archwiki-offline'"
         "$mainMod, T, exec, cliphist wipe"
         "$mainMod, SPACE, exec, rofi -show drun -width 65 -lines 15"
